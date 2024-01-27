b1be1d27524b7f2d1663389a2ba2a7ee4103cffc
kradhenz <tomas25498@gmail.com>
2024-01-27 05:10:57 -0300

    :sparkles: JS modified: smooth scroll, alert & tooltip added :art:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
-// SCROLL NAVBAR ANIMATION
-$(window).scroll(function(){
-    // CONDITIONAL STATEMENT BASED IN WINDOWS SIZE
+/* SCROLL NAVBAR ANIMATION */
+$(window).scroll(function () {
+    /* CONDITIONAL STATEMENT BASED IN WINDOWS SIZE */
     if (window.innerWidth < 768) {
         $('#nav').toggleClass('scrolled', $(this).scrollTop() > 100);
     } else {
         $('#nav').toggleClass('scrolled', $(this).scrollTop() > 600);
     }
-})
+});
 
-// CONTACT ALERT
-$("#send").click(function () {
-    alert("El mensaje fue enviado exitosamente");
-})
+$(document).ready(function () {
+    /* SMOOTH SCROLL ANIMATION */
+    $("a").click(function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+            let cat = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(cat).offset().top
+            }, 800, function () {
+                window.location.hash = cat;
+            });
+        }
+    });
+    /* CONTACT ALERT */
+    $("#send").click(function () {
+        alert("El mensaje fue enviado exitosamente");
+    })
+    /* TOOLTIP */
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+});
